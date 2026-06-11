dc78a1c23ea6111fb75fab71dee15fa67a7826e7
MARIALAADIMI <laadimilaadimi99@gmail.com>
2026-06-11 18:50:39 +0100

    ajout client

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,8 @@ Route::get('/produits', [ProduitController::class, 'index'])->name('produits.ind
 Route::post('/produit', [ProduitController::class, 'store'])->name('produits.store');
 Route::put('/produit/{produit}', [ProduitController::class, 'update'])->name('produits.update');
 Route::delete('/produit/{produit}', [ProduitController::class, 'destroy'])->name('produits.destroy');
+
+Route::get('/clients', [ClientController::class, 'index'])->name('clients.index');
+Route::post('/client', [ClientController::class, 'store'])->name('clients.store');
+Route::put('/client/{client}', [ClientController::class, 'update'])->name('clients.update');
+Route::delete('/client/{client}', [ClientController::class, 'destroy'])->name('clients.destroy');
